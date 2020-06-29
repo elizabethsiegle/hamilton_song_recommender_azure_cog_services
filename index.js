1c65b306561e9a7ba7ed055eb03376c7065abf0e
Lizzie Siegle <lizziedev24@gmail.com>
2020-06-29 14:54:36 -0700

    edit song descriptions

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
-const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
 exports.handler = async function(context, event, callback) {
 	let twiml = new Twilio.twiml.MessagingResponse();
 	const key = context.AZURE_KEY_HAMILFILM;
@@ -9,38 +8,37 @@ exports.handler = async function(context, event, callback) {
 	const input = [
         event.Body
     ];
-    
     const songs = {
         "non-stop": {
             desc: "Do not assume you are the smartest in the room. Time to do some work.",
             link: "youtube.com/watch?v=_YHVPNOHySk"
         },
         "wait for it": {
-            desc: "Good things take time. If you're doubting yourself, just keep going. You are inimitable, an original.",
+            desc: "Good things take time. You will get praise and validation soon. If you're doubting yourself, just keep going. You are inimitable, an original.",
             link: "youtube.com/watch?v=ulsLI029rH0"
         },
         "schuyler sisters": {
-            desc: "Girl power! You are empowered and thus empower others. Keep your siblings close. You may be looking for a mind at work.",
+            desc: "Girl power! You are empowered and thus empower others. Keep your siblings close. You may be looking for a significant other, a friend, a peer, or a general mind at work.",
             link: "youtube.com/watch?v=UeqKF_NF1Qs"
         },
         "dear theodosia": {
-            desc: "you're getting all emotional and teary over your kid or your pet. They're cute and have their whole lives ahead of them, which you will make better.",
+            desc: "you get all emotional and teary over your kid or your pet. They are cute, young, innocent, and have their whole lives ahead of them, which you will make better.",
             link: "youtube.com/watch?v=TKpJjdKcjeo"
         },
         "story of tonight": {
-            desc: "You're feeling emotional over what you, your friends, and your family will do in the future.",
+            desc: "You are feeling emotional over what you, your friends, and your family will do in the future. The night is still young. You can all do so much and change the world!",
             link: "youtube.com/watch?v=3vqwrepaMR0"
         },
         "my shot": {
-            desc: "life is tough but you are tougher. All you need is one chance, one shot per se, and here's the motivation you need to accomplish anything.",
+            desc: "life is tough but you are tougher. All you need is one chance, one shot per se, and here is the motivation you need to accomplish anything.",
             link: "youtube.com/watch?v=Ic7NqP_YGlg"
         },
         "alexander hamilton": {
-            desc: "You don't get the hype over Hamilton. This song will sum it up succinctly for you and you'll learn some history too.",
+            desc: "You don't get the hype over Hamilton. Hamilton may be new to you. This song will sum it up succinctly for you and you'll learn some history too.",
             link: "youtube.com/watch?v=VhinPd5RRJw"
         },
         "the reynolds pamphlet": {
-            desc: "have you read this? your archnemesis lost or is down. Someone you don't like was knocked out of the race. They are down but not out.",
+            desc: "your archnemesis lost or is down. Someone you don't like was knocked out of the race. They are down but not out.",
             link: "youtube.com/watch?v=uYQPP49MSEk"
         },
         "what'd i miss?": {
@@ -48,7 +46,7 @@ exports.handler = async function(context, event, callback) {
             link: "youtube.com/watch?v=vrnovNWC1f4"
         },
         "your obedient servant": {
-            desc: "it's okay to be petty and passive-aggressive sometimes. I mean, who doesn’t have an itemized list of thirty years of disagreements.",
+            desc: "it's okay to be petty and passive-aggressive sometimes. Some people deserve it because they are annoying. I mean, who doesn’t have an itemized list of thirty years of disagreements.",
             link: "youtube.com/watch?v=Yr-mO1o1uHk"
         },
         "helpless": {
@@ -64,7 +62,7 @@ exports.handler = async function(context, event, callback) {
             link: "youtube.com/watch?v=dSYW61XQZeo"
         },
         "take a break": {
-            desc: "You are obsessed with punctuation and thus by extension grammar and Oxford commas. We stan an intellectual but it is time to relax, take a break.",
+            desc: "You are obsessed with punctuation and put in a lot of hours at work. You are intellectual but it is time to relax, take a break.",
             link: "youtube.com/watch?v=D2UWNF8pKwk"
         },
         "the room where it happens": {
@@ -73,7 +71,6 @@ exports.handler = async function(context, event, callback) {
         },
     };
 
-    
     const sentimentResult = await textAnalyticsClient.analyzeSentiment(input);
     let sentiment, pos, neg, neutral, max;
     
@@ -101,5 +98,5 @@ exports.handler = async function(context, event, callback) {
     const reasonWhySong = songs[song].desc;
     const link = songs[song].link;
 	twiml.message(`You seem to be feeling ${max}. ${reasonWhySong} We recommend listening to ${song} right now: ${link}`);
-	callback(null, twiml);
+    callback(null, twiml);
 };
